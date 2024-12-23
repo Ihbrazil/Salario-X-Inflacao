193f6709ab8aff0b04cea5015b5ea7feab32c186
ihbrazil <ihbrazil@users.noreply.github.com>
2024-12-23 14:26:01 -0300

    Atualização da 6ª Missão da Carreira Programador Mobile - Construa o Aplicativo Salário x Inflação com React Native
    6ª Missão da Carreira Programador Mobile - Construa o Aplicativo Salário x Inflação com React Native - Aplicativo mobile que usa o recurso de props para transferir dados entre componentes. O app possui três telas e utiliza a navegação em abas.

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-export default function App() {
+import TelaSalario from './componentes/Salario';
+import TelaInflacao from './componentes/Inflacao';
+import TelaSalarioInflacao from './componentes/SalarioInflacao';
+
+import { MaterialIcons, Ionicons } from "@expo/vector-icons";
+
+const Abas = createBottomTabNavigator();
+
+export default function App () {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+
+      <Abas.Navigator>
+
+        <Abas.Screen
+          name="Tela Salário"
+          component = { TelaSalario }
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="attach-money" size={24} color={ color } />
+            ),
+          }}/>
+
+        <Abas.Screen
+          name="Tela Inflação"
+          component = { TelaInflacao }
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="trending-up" size={24} color={ color } />
+            ),
+          }} />
+        
+        <Abas.Screen
+          name="Tela Salário X Inflação"
+          component = { TelaSalarioInflacao }
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="compare-arrows" size={24} color={ color } />
+            )
+          }} />
+      </Abas.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
